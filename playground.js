ecf2bfab03b1ece042d3d83a3ff72001ebb2ef97
Dev10us <112738649+melonmasteristaken@users.noreply.github.com>
2024-07-22 14:47:19 +0800

    Create playground.js

---
+++ playground.js
@@ -0,0 +1,99 @@
+let auth = localStorage.getItem("auth");
+
+const xanoClient = new XanoClient({
+  instanceBaseUrl: "https://x8ki-letl-twmt.n7.xano.io/",
+  realtimeConnectionHash: "yHjeFpItI_ivBM9KL-tjZSYQAwM",
+});
+
+if (auth === 1) {
+  const setAuth = localStorage.getItem("authToken");
+  xano.setRealtimeAuthToken(authToken);
+}
+
+function generateRandomUsername() {
+  const adjectives = ['Awesome', 'Bold', 'Crazy', 'Daring', 'Energetic', 'Stealthy', 'Quick', 'Fierce'];
+  const nouns = ['Lion', 'Tiger', 'Bear', 'Wolf', 'Dragon', 'Fox', 'Serpent', 'Elephant'];
+  const randomAdjective = adjectives[Math.floor(Math.random() * adjectives.length)];
+  const randomNoun = nouns[Math.floor(Math.random() * nouns.length)];
+  return `${randomAdjective}${randomNoun}`;
+}
+
+var username
+
+if (auth == 1) {
+  fetch('https://x8ki-letl-twmt.n7.xano.io/api:iGbUspz7/auth/me', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + localStorage.getItem('authToken') // Use the stored JWT
+      }
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      username = data.name
+      // Set the innerHTML of the created HTML elements
+      //document.getElementById('username-display').innerHTML = data.name;
+      //document.getElementById('password-display').innerHTML = data.email;
+      //document.getElementById('timestamp').innerHTML = data.created_at; //'Account created at: ' + data.created_at;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+else {
+  username = generateRandomUsername();
+}
+
+const mainChannel = xanoClient.channel("main", {
+	presence:  true,
+});
+
+const messageInput = document.getElementById('message-input');
+const sendButton = document.getElementById('send-button');
+const messageList = document.getElementById('messageList');
+const messageListEmpty = document.getElementById('messageListEmpty');
+
+function isEmpty(element) { 
+  return element.innerHTML.trim() === '' 
+} 
+
+if (isEmpty(messageListEmpty)) {
+	messageListEmpty.insertAdjacentHTML('beforeend', '<p style="color: #af9cff;">Info: Start of console.</p>');
+}
+
+function displayMessage(message) {
+  console.log('Displaying message:', message);
+  messageList.insertAdjacentHTML('beforeend', message);
+  // Scroll to the bottom
+  messageList.scrollTop = messageList.scrollHeight;
+}
+
+
+sendButton.addEventListener('click', () => {
+	setTimeout(function(){
+		const message = messageInput.value;
+		checkForSpam(message); // Execute spam detection
+		displayMessage(`
+    <!--<p style="font-size: small; color: #e6e6e6;">${username}</p>-->
+    <p>${message}</p>
+  `;);
+		messageInput.value = ''; // Clear input field
+	}, 200);
+});
+
+
+// Check if signed in via Xano
+
+function updateAuthStatus() {
+  auth = localStorage.getItem("auth");
+  if (auth === '1') {
+    document.getElementById("auth0").style.display = "none";
+    document.getElementById("auth1").style.display = "block";
+  } else {
+    document.getElementById("auth0").style.display = "block";
+    document.getElementById("auth1").style.display = "none";
+  }
+}
+
+// Call updateAuthStatus() when the page loads
+updateAuthStatus();
